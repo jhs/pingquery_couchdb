e0f8947f83bb5976ba9989db6e7c6a73f44f5855
Jason Smith <jhs@couchone.com>
2010-12-17 21:03:33 +0700

    Change to throw() instead of deep nesting case expressions

--- src/pingquery_couchdb.erl
+++ src/pingquery_couchdb.erl
@@ -14,51 +14,13 @@ handle_pingquery_req(Req=#httpd{method='POST'})
     -> ?LOG_DEBUG("Received ping request: ~p", [Req])
     , couch_httpd:validate_ctype(Req, "application/json")
     , ok = couch_httpd:verify_is_server_admin(Req)
-    , case get_ping_spec(Req)
-        of {error, Reason}
+    , try get_ping_spec(Req)
+        of {ok, Language, Code, ExpectedResult}
+            -> ping_query_server(Req, Language, Code, ExpectedResult)
+        catch throw:{error, Reason}
             -> send_bad_query(Req, Reason)
-        ; {error, Reason, Extras}
+        ; throw:{error, Reason, Extras}
             -> send_bad_query(Req, Reason, Extras)
-        ; {ok, Language, Code, ExpectedResult}
-            -> ?LOG_DEBUG("Ping in ~s: ~s -> \"~s\"", [Language, Code, ExpectedResult])
-            % The idea is to create a fresh design doc for every query to guarantee that the view server has to evaluate/compile the code and run it.
-            , Id = <<"_design/pingquery", "(Also, hopefully nobody will ever use this doc id!)">>
-            , RevHash = couch_uuids:random()
-            , Revs = {1, [RevHash]}
-            , DDocBody = {[ {<<"_id">>, Id}
-                          , {<<"_rev">>, iolist_to_binary(["1-", RevHash])}
-                          , {<<"language">>, Language}
-                          , {<<"shows">>, {[ {<<"pingquery">>, Code} ]}}
-                          ]}
-            , DDoc = #doc{id=Id, revs=Revs, body=DDocBody}
-            , ?LOG_DEBUG("DDoc: ~p", [DDoc])
-            , JsonReq = stub_req_obj(Req)
-            , ?LOG_DEBUG("Req: ~p", [JsonReq])
-            , try couch_query_servers:ddoc_prompt(DDoc, [<<"shows">>, <<"pingquery">>], [{[]}, JsonReq])
-                of [<<"resp">>, {[{<<"body">>, Response}]}]
-                    -> ?LOG_DEBUG("Response from ping execution: ~p", [Response])
-                    , case Response
-                        of ExpectedResult
-                            -> couch_httpd:send_json(Req, 200, {[{<<"ok">>, true}, {<<"match">>, Response}]})
-                        ; BadResult
-                            -> ?LOG_DEBUG("Bad ping match; Expected=~p Result=~p", [ExpectedResult, BadResult])
-                            , send_bad_ping(Req, "no_match", {[{<<"expected">>, ExpectedResult}, {<<"received">>, BadResult}]})
-                        end
-                ; Else
-                    -> ?LOG_ERROR("Unexpected response from view server prompt: ~p", [Else])
-                    , send_bad_ping(Req, Else)
-                catch throw:{<<"render_error">>, <<"undefined response from show function">>}
-                    -> send_bad_ping(Req, "Ping function must return a string")
-                ; throw:{<<"render_error">>, Reason}
-                    -> send_bad_ping(Req, Reason)
-                ; throw:{<<"compilation_error">>, Reason}
-                    -> send_bad_query(Req, Reason)
-                ; throw:{unknown_query_language, BadLang}
-                    -> send_bad_query(Req, [BadLang, " is not a known query language"])
-                ; Class:Exc
-                    -> ?LOG_ERROR("Uncaught exception handling ddoc prompt: ~p:~p", [Class, Exc])
-                    , send_bad_ping(Req, io_lib:format("Erlang exception: ~p:~p", [Class, Exc]))
-                end
         end
     ;
 
@@ -66,46 +28,94 @@ handle_pingquery_req(Req)
     -> couch_httpd:send_method_not_allowed(Req, "POST")
     .
 
-stub_req_obj(_Req)
+ping_query_server(Req, Language, Code, ExpectedResult)
+    -> ?LOG_DEBUG("Ping in ~s: ~s -> \"~s\"", [Language, Code, ExpectedResult])
+    % The idea is to create a fresh design doc for every query to guarantee that the view server has to evaluate/compile the code and run it.
+    , Id = <<"_design/pingquery", "(Also, hopefully nobody will ever use this doc id!)">>
+    , RevHash = couch_uuids:random()
+    , Revs = {1, [RevHash]}
+    , DDocBody = {[ {<<"_id">>, Id}
+                  , {<<"_rev">>, iolist_to_binary(["1-", RevHash])}
+                  , {<<"language">>, Language}
+                  , {<<"shows">>, {[ {<<"pingquery">>, Code} ]}}
+                  ]}
+    , DDoc = #doc{id=Id, revs=Revs, body=DDocBody}
+    , ?LOG_DEBUG("DDoc: ~p", [DDoc])
+    , JsonReq = stub_req_obj(Req)
+    , ?LOG_DEBUG("Req: ~p", [JsonReq])
+    , try couch_query_servers:ddoc_prompt(DDoc, [<<"shows">>, <<"pingquery">>], [{[]}, JsonReq])
+        of [<<"resp">>, {[{<<"body">>, Response}]}]
+            -> ?LOG_DEBUG("Response from ping execution: ~p", [Response])
+            , case Response
+                of ExpectedResult
+                    -> couch_httpd:send_json(Req, 200, {[{<<"ok">>, true}, {<<"match">>, Response}]})
+                ; BadResult
+                    -> ?LOG_DEBUG("Bad ping match; Expected=~p Result=~p", [ExpectedResult, BadResult])
+                    , send_bad_ping(Req, "no_match", {[{<<"expected">>, ExpectedResult}, {<<"received">>, BadResult}]})
+                end
+        ; Else
+            -> ?LOG_ERROR("Unexpected response from view server prompt: ~p", [Else])
+            , send_bad_ping(Req, Else)
+        catch throw:{<<"render_error">>, <<"undefined response from show function">>}
+            -> send_bad_ping(Req, "Ping function must return a string")
+        ; throw:{<<"render_error">>, Reason}
+            -> send_bad_ping(Req, Reason)
+        ; throw:{<<"compilation_error">>, Reason}
+            -> send_bad_query(Req, Reason)
+        ; throw:{unknown_query_language, BadLang}
+            -> send_bad_query(Req, [BadLang, " is not a known query language"])
+        ; Class:Exc
+            -> ?LOG_ERROR("Uncaught exception handling ddoc prompt: ~p:~p", [Class, Exc])
+            , send_bad_ping(Req, io_lib:format("Erlang exception: ~p:~p", [Class, Exc]))
+        end
+    .
+
+stub_req_obj(Req)
     % TODO , JsonReq = couch_httpd_external:json_req_obj(Req, #db{})
     -> {[{<<"req">>, <<"not implemented">>}]}
+    %, couch_httpd_external:json_req_obj(Req, #db{})
     .
 
 get_ping_spec(Req=#httpd{path_parts=PathParts})
     -> Example = {[{<<"example">>, {[{<<"in">>, <<"function() { return 'Hello, world!'; }">>}, {<<"out">>, <<"Hello, world!">>}]}}]}
-    , case PathParts
-        of [_HandlerTrigger, Language | _Rest]
-            -> ?LOG_DEBUG("Requested query language: ~s", [Language])
-            , try couch_httpd:json_body(Req)
-                of {PingSpec}
-                    -> ?LOG_DEBUG("Valid JSON body: ~p", [{PingSpec}])
-                    , case couch_util:get_value(<<"in">>, PingSpec)
-                        of undefined
-                            -> {error, "Required 'in' value; see example", Example}
-                        ; BadVal when is_binary(BadVal) =:= false
-                            -> {error, "'in' value must be a string; see example", Example}
-                        ; Code
-                            -> case couch_util:get_value(<<"out">>, PingSpec)
-                                of undefined
-                                    -> {error, "Required 'out' value; see example", Example}
-                                ; BadVal when is_binary(BadVal) =:= false
-                                    -> {error, "'out' value must be a string; see example", Example}
-                                ; ExpectedResult
-                                    % Success!
-                                    -> {ok, Language, Code, ExpectedResult}
-                                end
-                        end
-                catch throw:{invalid_json, undefined}
-                    -> {error, "JSON body required; see the example", Example}
-                ; throw:{invalid_json, _}
-                    -> {error, "Invalid JSON; see the example", Example}
-                ; Class:Exc
-                    -> ?LOG_ERROR("Uncaught exception processing POST body: ~p:~p", [Class, Exc])
-                    , {error, io_lib:format("Erlang exception processing query: ~p:~p", [Class, Exc])}
-                end
-        ; _
-            -> {error, "Language required; e.g. /_pingquery/javascript"}
+    , Language = case length(PathParts)
+        of X when X < 2 -> throw({error, "Language required; e.g. /_pingquery/javascript"})
+        ; _             -> lists:nth(2, PathParts)
         end
+
+    , ?LOG_DEBUG("Requested query language: ~s", [Language])
+    , PingSpec = try couch_httpd:json_body(Req)
+        of {Body} -> Body
+        catch throw:{invalid_json, undefined}
+            -> throw({error, "JSON body required; see the example", Example})
+        ; throw:{invalid_json, _}
+            -> throw({error, "Invalid JSON; see the example", Example})
+        ; Class:Exc
+            -> ?LOG_ERROR("Uncaught exception processing POST body: ~p:~p", [Class, Exc])
+            , throw({error, io_lib:format("Erlang exception processing query: ~p:~p", [Class, Exc])})
+        end
+
+    , ?LOG_DEBUG("Valid JSON body: ~p", [{PingSpec}])
+    , Code = case couch_util:get_value(<<"in">>, PingSpec)
+        of undefined
+            -> throw({error, "Required 'in' value; see example", Example})
+        ; BadCode when is_binary(BadCode) =:= false
+            -> throw({error, "'in' value must be a string; see example", Example})
+        ; CodeVal
+            -> CodeVal
+        end
+
+    , ExpectedResult = case couch_util:get_value(<<"out">>, PingSpec)
+        of undefined
+            -> throw({error, "Required 'out' value; see example", Example})
+        ; BadResult when is_binary(BadResult) =:= false
+            -> throw({error, "'out' value must be a string; see example", Example})
+        ; ER
+            -> ER
+        end
+
+    % Success!
+    , {ok, Language, Code, ExpectedResult}
     .
 
 send_bad_query(Req, Reason)
